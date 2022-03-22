14953a662e403e9e530831178098c1045cbb5574
JBEDU18 <JBEDU18@DESKTOP-AELTP4L>
2022-03-22 13:23:57 +0900

    배열 연습

---
+++ src/jw0322_beyel/beyel.java
@@ -0,0 +1,24 @@
+package jw0322_beyel;
+
+import java.util.*;
+
+public class beyel {
+
+	public static void main(String[] args) {
+		//TODO Auto-generated method stub
+		int intArray[] = new int[5];
+		intArray[0] = 5;
+		
+		int arr1[] = new int[5];
+		int arr_length = arr1.length;
+		
+		System.out.print(arr_length);
+		for(int i=0;i<arr_length;i++) {
+		}
+		}
+		
+		//intArray[6] = 10; //인덱스 범위를 벗어남으로 오류주의!!
+		
+//		System.out.print(intArray[3]);
+	}
+
